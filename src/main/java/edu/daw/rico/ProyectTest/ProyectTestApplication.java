0b18b42986baa2bac6f78ffeda95793b622ebd72
Manuel Rico <manuelricolopez@liceolapaz.net>
2025-09-25 10:59:05 +0200

    Empezando ProyectoTest

---
+++ src/main/java/edu/daw/rico/ProyectTest/ProyectTestApplication.java
@@ -0,0 +1,13 @@
+package edu.daw.rico.ProyectTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/daw/rico/ProyectTest/ProyectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.daw.rico.ProyectTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
